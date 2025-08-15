d336ef91414210dc8627cce8d4914f37f653bd35
Кирилл Гуляев <gki-webik@ya.ru>
2025-08-15 18:44:32 +0600

    add

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,12 @@ export default function RootLayout({ children }) {
           name="viewport"
           content="width=device-width, initial-scale=1"
         ></meta>
+        <link rel="preload" as="image" href="/media/bg.png" />
+        <link rel="preload" as="image" href="/media/clock.svg" />
+        <link rel="preload" as="image" href="/media/ispeople.png" />
+        <link rel="preload" as="image" href="/media/ispeople-2.png" />
+        <link rel="preload" as="image" href="/media/loader.png" />
+        <link rel="preload" as="image" href="/media/maskLoader.png" />
       </head>
       <body>
         <div className="max_container">{children}</div>
